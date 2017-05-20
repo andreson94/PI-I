811ac12f11a20babfb6da02730d7ac2782cb732d
andreson.csilva <andreson.csilva@CAS555589W0736.senacsp.edu.br>
2017-05-19 22:46:34 -0300

    funcao

---
+++ ALPI/src/Exe1.java
@@ -0,0 +1,50 @@
+
+
+import java.util.Scanner;
+
+public class Exe1 {
+     static Scanner input = new Scanner (System.in);
+     static int menu(){
+         int opcao;
+         System.out.println("**Menu**");
+         System.out.println("1-adição");
+         System.out.println("2-subtração");
+         System.out.println("3-multiplicação");
+         System.out.println("4-divisão");
+         System.out.println("Opção: ");
+         opcao = input.nextInt();
+         return opcao;
+    }
+     static int add(int x, int y){
+         System.out.println("**add**");
+         int resultado = x+y;
+         return resultado;
+     }
+     
+     static int entradedeDados(){
+         System.out.println("**entrada**");
+         int n = input.nextInt();
+         return n;
+     }
+     static void imprimir(double n){
+         System.out.println("**imprimir**");
+         System.out.println("resultado: "+ n);
+     }
+     static double controle(int opcao, int x, int y){
+         System.out.println("**controle**");
+         double r=0;
+         switch(opcao){
+             case 1:
+                 r = add(x,y);
+                 //continuar o codigo
+         }
+         return r;
+     }
+     public static void main(String[] args) {
+        int opcao = menu();
+        int x = entradedeDados();
+        int y = entradedeDados();
+        double r = controle(opcao, x, y);
+         imprimir(r);
+    }
+}
---
+++ ALPI/src/ExerciciosFuncao/teste.java
@@ -0,0 +1,28 @@
+
+package ExerciciosFuncao;
+public class teste{
+     static String s (){
+         String status = null;
+         System.out.println("Aprovado");
+         System.out.println("Reprovado");
+    return status;
+    }
+    static void imprimirNota (double nota){
+   
+    if (nota >= 6){
+       String s = status(); 
+    }
+    else {
+      String s = status();
+    }
+    System.out.println(status());
+    }
+        public static void main (String [] args){
+            imprimirNota(6.7);
+            String status = status();
+            System.out.println(status);
+        
+       }
+
+    }
+    }
