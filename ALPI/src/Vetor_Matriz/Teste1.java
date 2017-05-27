73a243e8d59e2806fad21ab02f16a6a78e9519c5
andreson.csilva <andreson.csilva@CAS555589W0736.senacsp.edu.br>
2017-05-26 23:02:42 -0300

    atualizacao4

--- ALPI/src/Vetor_Matriz/Teste1.java
+++ ALPI/src/Vetor_Matriz/Teste1.java
@@ -1,14 +1,45 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Vetor_Matriz;
 
-/**
- *
- * @author andreson.csilva
- */
+import java.util.Scanner;
+
 public class Teste1 {
-    
+    public static void main(String[] args) {
+        Scanner imput = new Scanner(System.in);
+        int n = 10;
+        int[] v = new int[n];
+        
+        for (int i = 0; i < n; i++){
+            System.out.print("Digite um numero: ");
+            v[i] = imput.nextInt();
+            
+        }
+        
+        int soma = 0;
+        int menor = 0;
+        int maior = 0;
+        
+        for (int i = 0; i < n; i++){
+            soma = soma + v[i];
+            
+            if (v[i] < menor){
+                menor = v[i];
+            }
+            if(v[i]> maior){
+                maior = v[i];
+            }
+        }
+            for (int i = 0; i < n; i++){
+                System.out.print("v[" + i + "]: " + v[i]);
+                
+                if (v[i] == menor)
+                    System.out.print(" <-- menor");
+                else if (v[i] == maior)
+                    System.out.print(" <-- maior"); 
+                    System.out.println(" ");
+                
+            }
+            System.out.println("A soma é: " + soma);
+    }
 }
+
---
+++ ALPI/src/Vetor_Matriz/teste2.java
@@ -0,0 +1,89 @@
+package Vetor_Matriz;
+
+import java.util.Scanner;
+public class teste2 {
+    static Scanner imput = new Scanner(System.in);
+     
+    static int entradadeDados (int n){
+       System.out.println("Digite um numero: ");
+
+       n = imput.nextInt();
+
+       return n;
+   }
+    static int[] criaVetor (int n){
+        
+        int[] v = new int[n];
+        
+       return v; 
+    
+}
+    static int[] preencheVetor (int[] v){
+        System.out.println("preenchimento do vetor");
+        int n = entradadeDados(0);
+        for (int i = 0; i < n; i++){
+            System.out.print("Digite um numero: ");
+            v[i] = imput.nextInt();
+            
+        }
+       return v;
+    }
+    static int maior (int[] v){
+        int maior = 0;
+        
+        for (int i = 0; i < v.length; i++){
+            if(v[i]> maior){
+                maior = v[i];
+            }
+        }
+        return maior;
+    } 
+      static int somaN (int[] v){
+        int soma = 0;
+               
+        for (int i = 0; i < v.length; i++){
+            soma = soma + v[i];
+            
+        }
+        return soma;
+    } 
+        static int menorM (int[] v){
+        int menor = 0;
+        
+        for (int i = 0; i < v.length; i++){
+            
+            if (v[i] < menor){
+                menor = v[i];
+            }
+        }
+        return menor;
+    } 
+         
+    static int fim (int[] v,int menor,int maior,int soma){
+     
+        int resultado;
+        
+              for (int i = 0; i < v.length; i++){
+                System.out.print("v[" + i + "]: " + v[i]);
+            
+                
+                if (v[i] == menor)
+                    System.out.print(" <-- menor");
+                else if (v[i] == maior)
+                    System.out.print(" <-- maior"); 
+                    System.out.println(" ");
+                
+            }
+            System.out.println("A soma é: " + soma);
+        return resultado;
+    } 
+          
+    public static void main(String[] args) {
+        
+      
+            
+    }
+}
+
+
+
